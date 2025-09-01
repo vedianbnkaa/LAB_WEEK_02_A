91c8d757aaedfafae5ae0e92d9fd5c3edfac2786
vedianbnka <v.bunka238@gmail.com>
2025-09-01 16:21:54 +0700

    Commit No. 1 – Activity lifecycle

--- app/src/main/java/com/vedianbunka/lab_week_02_a/MainActivity.kt
+++ app/src/main/java/com/vedianbunka/lab_week_02_a/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.vedianbunka.lab_week_02_a
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onRestart (){
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onStart (){
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onPause (){
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onResume (){
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onStop (){
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy (){
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 }
